dfa5dbb6a6d8649a92ae77c55622e9bab7b9a726
Deivid <ppdeividpp12@gmail.com>
2025-12-08 23:39:12 -0300

    new version

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    webpack: (config, { isServer }) => {
+        config.externals = config.externals || {};
+        if (!isServer) {
+            config.externals['debug'] = 'debug';
+            config.externals['supports-color'] = 'supports-color';
+        }
+        return config;
+    }
 }
 
 module.exports = nextConfig
